54f94d0ce80125ba371ad2c3c5923a6201af50d2
arixsv <arixsv@gmail.com>
2023-07-15 23:20:21 +0700

    Menampilkan Analisis pada Dashboard Admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,38 +4,102 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-md-12 grid-margin">
-      @if(session('message'))
-              <h6 class="alert alert-success">{{ session('message') }}</h6>
-      @endif
+    <div class="row">
+        <div class="col-md-12 grid-margin">
 
-      <div class="d-flex justify-content-between flex-wrap">
-        <div class="d-flex align-items-end flex-wrap">
-          <div class="me-md-3 me-xl-5">
-            <h2>Dashboard,</h2>
-            <p class="mb-md-0">Your analytics dashboard template.</p>
-          </div>
-          <div class="d-flex">
-            <i class="mdi mdi-home text-muted hover-cursor"></i>
-            <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
-            <p class="text-primary mb-0 hover-cursor">Analytics</p>
-          </div>
+            @if (session('message'))
+                <h6 class="alert alert-success">{{ session('message') }}</h6>
+            @endif
+
+            <div class="me-md-3 me-xl-5">
+                <h2>Dashboard,</h2>
+                <p class="mb-md-0">Dashboard Analisis</p>
+                <hr>
+            </div>
+
+            <div class="row">
+                <h4>Data Pembelian :</h4>
+                <div class="col-md-3">
+                    <div class="card card-body bg-primary text-white mb-3" style="border-radius: 12px;">
+                        <label>Total Pembeli</label>
+                        <h1>{{ $totalOrder }}</h1>
+                        <a href="{{ url('admin/orders') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-body bg-warning text-white mb-3" style="border-radius: 12px;">
+                        <label>Pembeli Hari Ini</label>
+                        <h1>{{ $todayOrder }}</h1>
+                        <a href="{{ url('admin/orders') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-body bg-danger text-white mb-3" style="border-radius: 12px;">
+                        <label>Pembeli Bulan Ini</label>
+                        <h1>{{ $thisMonthOrder }}</h1>
+                        <a href="{{ url('admin/orders') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-body bg-success text-white mb-3" style="border-radius: 12px;">
+                        <label>Pembeli Tahun Ini</label>
+                        <h1>{{ $thisYearOrder }}</h1>
+                        <a href="{{ url('admin/orders') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+            </div>
+            <br />
+            <!-- <div class="row">
+                <h4>Data Produk :</h4>
+                <div class="col-md-3">
+                    <div class="card card-body bg-warning text-white mb-3" style="border-radius: 12px;">
+                        <label>Total Produk</label>
+                        <h1>{{ $totalProducts }}</h1>
+                        <a href="{{ url('admin/products') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-body bg-danger text-white mb-3" style="border-radius: 12px;">
+                        <label>Total Kategori</label>
+                        <h1>{{ $totalCategories }}</h1>
+                        <a href="{{ url('admin/category') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-body bg-success text-white mb-3" style="border-radius: 12px;">
+                        <label>Total Brands</label>
+                        <h1>{{ $totalBrands }}</h1>
+                        <a href="{{ url('admin/brands') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+            </div>
+            <br /> -->
+            <!-- <div class="row">
+                <h4>Data User :</h4>
+                <div class="col-md-3">
+                    <div class="card card-body bg-danger text-white mb-3" style="border-radius: 12px;">
+                        <label>Total Semua User</label>
+                        <h1>{{ $totalAllUsers }}</h1>
+                        <a href="{{ url('admin/users') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-body bg-success text-white mb-3" style="border-radius: 12px;">
+                        <label>Total User</label>
+                        <h1>{{ $totalUser }}</h1>
+                        <a href="{{ url('admin/users') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-body bg-primary text-white mb-3" style="border-radius: 12px;">
+                        <label>Total Admin</label>
+                        <h1>{{ $totalAdmin }}</h1>
+                        <a href="{{ url('admin/users') }}" class="text-white text-end">Lihat</a>
+                    </div>
+                </div>
+            </div> -->
+
         </div>
-        <div class="d-flex justify-content-between align-items-end flex-wrap">
-          <button type="button" class="btn btn-light bg-white btn-icon me-3 d-none d-md-block ">
-            <i class="mdi mdi-download text-muted"></i>
-          </button>
-          <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
-            <i class="mdi mdi-clock-outline text-muted"></i>
-          </button>
-          <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
-            <i class="mdi mdi-plus text-muted"></i>
-          </button>
-          <button class="btn btn-primary mt-2 mt-xl-0">Generate report</button>
-        </div>
-      </div>
     </div>
-</div>
 
 @endsection
